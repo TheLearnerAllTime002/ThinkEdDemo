158ac0d46e11affc1c6a1b3e066e197cf4953efe
TheLearnerAllTime002 <arjunmitra06082004@gmail.com>
2025-09-09 07:36:24 +0530

    vercel comp

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { LanguageProvider } from "./contexts/LanguageContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import BackgroundEffects from "./components/layout/BackgroundEffects";
 import { performanceMonitor } from "./utils/animationConfig";
+import SpeedInsights from '@vercel/speed-insights/react';
 
 function App() {
   useEffect(() => {
@@ -23,23 +24,26 @@ function App() {
   }, []);
 
   return (
-    <ThemeProvider>
-      <LanguageProvider>
-        <AppProvider>
-          <AuthProvider>
-            <div className="relative min-h-screen">
-            {/* Global Background Effects */}
-            <BackgroundEffects variant="minimal" className="fixed inset-0 z-0" />
-            
-            {/* Main Application */}
-            <div className="relative z-10">
-              <Routes />
+    <>
+      <ThemeProvider>
+        <LanguageProvider>
+          <AppProvider>
+            <AuthProvider>
+              <div className="relative min-h-screen">
+              {/* Global Background Effects */}
+              <BackgroundEffects variant="minimal" className="fixed inset-0 z-0" />
+              
+              {/* Main Application */}
+              <div className="relative z-10">
+                <Routes />
+              </div>
             </div>
-          </div>
-          </AuthProvider>
-        </AppProvider>
-      </LanguageProvider>
-    </ThemeProvider>
+            </AuthProvider>
+          </AppProvider>
+        </LanguageProvider>
+      </ThemeProvider>
+      <SpeedInsights />
+    </>
   );
 }
 
