e2eeb9b3ded1eb651555a985f7e8ce1181d872f7
Arjun Mitra <arjunmitra06082004@gmail.com>
2025-09-13 15:31:40 +0530

    Update ContactFooter.jsx

--- src/pages/landing-page/components/ContactFooter.jsx
+++ src/pages/landing-page/components/ContactFooter.jsx
@@ -1,48 +1,53 @@
 import { useAnimationConfig } from '../../../hooks/useReducedMotion';
+import { AiOutlineMail } from 'react-icons/ai';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6'; // "X" rebrand for Twitter
+
 
 const ContactFooter = () => {
   const navigate = useNavigate();
   const currentYear = new Date()?.getFullYear();
   const { getAnimationProps } = useAnimationConfig();
 
-  const socialLinks = [
-    {
-      name: "Gmail",
-      icon: "Mail",
-      href: "mailto:arjunmitra003@gmail.com",
-      color: "text-red-500",
-      hoverColor: "hover:bg-red-500/10",
-      description: "Send us an email",
-      gradient: "from-red-500 to-red-600"
-    },
-    {
-      name: "LinkedIn",
-      icon: "Linkedin",
-      href: "https://www.linkedin.com/in/arjun-mitra-2761a9260/",
-      color: "text-blue-600",
-      hoverColor: "hover:bg-blue-600/10",
-      description: "Connect with us",
-      gradient: "from-blue-600 to-blue-700"
-    },
-    {
-      name: "GitHub",
-      icon: "Github",
-      href: "https://github.com/TheLearnerAllTime002/ThinkEdDemo",
-      color: "text-gray-800 dark:text-gray-200",
-      hoverColor: "hover:bg-gray-800/10 dark:hover:bg-gray-200/10",
-      description: "View our code",
-      gradient: "from-gray-800 to-gray-900 dark:from-gray-200 dark:to-gray-300"
-    },
-    {
-      name: "Twitter",
-      icon: "Twitter",
-      href: "https://x.com/ArjunMitra15888",
-      color: "text-blue-400",
-      hoverColor: "hover:bg-blue-400/10",
-      description: "Follow us",
-      gradient: "from-blue-400 to-blue-500"
-    }
-  ];
+ const socialLinks = [
+  {
+    name: "Gmail",
+    icon: AiOutlineMail,
+    href: "mailto:arjunmitra003@gmail.com",
+    color: "text-red-500",
+    hoverColor: "hover:bg-red-500/10",
+    description: "Send us an email",
+    gradient: "from-red-500 to-red-600",
+  },
+  {
+    name: "LinkedIn",
+    icon: FaLinkedin,
+    href: "https://www.linkedin.com/in/arjun-mitra-2761a9260/",
+    color: "text-blue-600",
+    hoverColor: "hover:bg-blue-600/10",
+    description: "Connect with us",
+    gradient: "from-blue-600 to-blue-700",
+  },
+  {
+    name: "GitHub",
+    icon: FaGithub,
+    href: "https://github.com/TheLearnerAllTime002/ThinkEdDemo",
+    color: "text-gray-800 dark:text-gray-200",
+    hoverColor: "hover:bg-gray-800/10 dark:hover:bg-gray-200/10",
+    description: "View our code",
+    gradient: "from-gray-800 to-gray-900 dark:from-gray-200 dark:to-gray-300",
+  },
+  {
+    name: "Twitter",
+    icon: FaXTwitter,
+    href: "https://x.com/ArjunMitra15888",
+    color: "text-blue-400",
+    hoverColor: "hover:bg-blue-400/10",
+    description: "Follow us",
+    gradient: "from-blue-400 to-blue-500",
+  }
+];
+
 
   const quickLinks = [
     { label: "Home", path: "/landing-page", icon: "Home" },
